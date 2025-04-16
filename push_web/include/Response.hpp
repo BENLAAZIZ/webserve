2baec0a15902f0b11958de4cdf44491534e90728
Hamza Ben laziz <benlaaziz.hamza@gmail.com>
2025-04-16 18:01:16 +0100

    update webserv

---
+++ push_web/include/Response.hpp
@@ -0,0 +1,63 @@
+
+#ifndef RESPONSE_HPP
+#define RESPONSE_HPP
+
+
+#include "web.h"
+
+
+class Response {
+    public:
+
+        std::ifstream	file;
+		std::ofstream	filetest;
+		std::string		_responseBuffer;
+		bool			_responseSent;
+		bool			_keepAlive;
+		bool			_header_falg;
+		bool			_isopen;
+		int				_clientFd;
+		size_t			_fileOffset;
+        int				fd;
+        std::string		fullPath;
+        int flag_p;
+        int is_file;
+        int is_dir;
+        int bytes_sent;
+        off_t			fileSize;
+
+        // const size_t CHUNK_SIZE = 1024;
+        size_t CHUNK_SIZE;
+
+
+    public:
+        Response();
+        ~Response();
+        Response(const Response& other);
+		Response& operator=(const Response& other);
+
+        // Methods
+
+    
+        bool        keepAlive() const;
+        void        handleGetResponse(int *flag, Request &request);
+        void        reset();
+        std::string	get_error_missage(int errorCode) const;
+        std::string	get_code_error_path(int errorCode) const;
+        std::string get_MimeType (const std::string& path);
+        void        send_header_response(size_t CHUNK_SIZE, std::string path, Request &request, int flag_autoindex);
+        int         send_file_response(char *buffer, int bytes_read);
+        int         open_file(int *flag, std::string fullPath, int *code);
+        //=============
+
+        void type_of_path(std::string& path);// mo2a
+        void send_header_response_autoIndex(std::string path, Request &request);
+
+        bool send_Error_Response(int client_fd);
+        void there_is_error_file(std::string fullPath, int statusCode);
+        void generate_default_error_response(int statusCode);
+        void generate_error_response(int statusCode,  int client_fd, Server_holder& serv_hldr);
+
+};
+
+#endif // RESPONSE_HPP
---
+++ push_web/include/Server.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 13:05:47 by hben-laz          #+#    #+#             */
+/*   Updated: 2025/04/16 17:47:03 by hben-laz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include "web.h"
+
+class Client;
+class ConfigFile;
+
+#define BUFFER_SIZE 8000
+#define MAX_CLIENTS 1000
+#define MAX_REQUEST_SIZE 1000000
+
+class Server {
+    public:
+        ConfigFile *_config; 
+        // Server(int port);
+        Server(int port, Server_holder& serv_hldr);
+        ~Server();
+        int getServerFd() const { return server_fd; }
+        int getPort() const { return port; }
+        int acceptNewConnection();
+        int handleClientData(int client_fd, Client &client);
+        void setNonBlocking(int fd);
+        void setConfig(ConfigFile* config); // Add setter method
+        int sendResponse(int client_fd, Client &client);
+        void sendSuccessResponse(int clientSocket);
+
+        int handleResponse(int client_fd, Client &client);
+        
+        Server_holder serv_hldr;
+        
+    private:
+        int server_fd;
+        int port;
+};
+
+#endif
